394c3eb94940204fe935e1a049d13f66d9221102
Armin Kuhestani <armin_kuhestani@apple.com>
2019-08-02 11:22:48 +1000

    Added functions to update state depending on input

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,11 +1,49 @@
-import React, { Component } from "react"
-import { View } from "react-native"
-import { getMetricMetaInfo } from "../utils/helpers"
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import { getMetricMetaInfo } from '../utils/helpers'
 
 const GetMetricMetaInfo = getMetricMetaInfo("bike").getIcon
 
 export default class AddEntry extends Component {
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0,
+  }
+  increment = (metric) => {
+    const { max, step } = getMetricMetaInfo(metric)
+
+    this.setState((state) => {
+      const count = state[metric] + step
+
+      return {
+        ...state,
+        [metric]: count > max ? max : count,
+      }
+    })
+  }
+  decrement = (metric) => {
+    this.setState((state) => {
+      const count = state[metric] - getMetricMetaInfo(metric).step
+
+      return {
+        ...state,
+        [metric]: count < 0 ? 0 : count,
+      }
+    })
+  }
+  slide = (metric, value) => {
+    this.setState(() => ({
+      [metric]: value
+    }))
+  }
   render() {
-    return <View><GetMetricMetaInfo /></View>
+    return (
+      <View>
+          <GetMetricMetaInfo />
+      </View>
+    )
   }
 }
