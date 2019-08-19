5fb9e6b0f25e2c80cc73a2ee0d3831dcc41d2a6c
Armin Kuhestani <armin_kuhestani@apple.com>
2019-08-20 08:59:12 +1000

    Added styling and platform specific icons

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, TouchableOpacity, Text } from 'react-native'
+import { View, TouchableOpacity, Text, StyleSheet, Platform } from 'react-native'
 import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
 import MySlider from './MySlider'
 import Steppers from './Steppers'
@@ -9,15 +9,15 @@ import TextButton from './TextButton';
 import { submitEntry, removeEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
+import { purple, white } from "../utils/colors"
 
 
 function SubmitBtn ({ onPress }) {
-    return (
-      <TouchableOpacity
-        onPress={onPress}>
-          <Text>SUBMIT</Text>
-      </TouchableOpacity>
-    )
+  return (
+    <TouchableOpacity style={Platform.OS === "ios" ? styles.iosSubmitBtn : styles.AndroidSubmitBtn} onPress={onPress}>
+      <Text style={styles.submitBtnText}>SUBMIT</Text>
+    </TouchableOpacity>
+  )
 }
 
 class AddEntry extends Component {
@@ -27,29 +27,29 @@ class AddEntry extends Component {
     bike: 0,
     swim: 0,
     sleep: 0,
-    eat: 0,
+    eat: 0
   }
 
-  increment = (metric) => {
+  increment = metric => {
     const { max, step } = getMetricMetaInfo(metric)
 
-    this.setState((state) => {
-      const count = state[metric] + step
+    this.setState(state => {
+      const count = state[metric] + step;
 
       return {
         ...state,
-        [metric]: count > max ? max : count,
+        [metric]: count > max ? max : count
       }
     })
   }
 
-  decrement = (metric) => {
-    this.setState((state) => {
+  decrement = metric => {
+    this.setState(state => {
       const count = state[metric] - getMetricMetaInfo(metric).step
 
       return {
         ...state,
-        [metric]: count < 0 ? 0 : count,
+        [metric]: count < 0 ? 0 : count
       }
     })
   }
@@ -88,23 +88,23 @@ class AddEntry extends Component {
 
     if (this.props.alreadyLogged) {
       return (
-        <View>
-          <Ionicons name={'ios-happy'} size={100} />
+        <View style={styles.center}>
+          <Ionicons name={Platform.OS === "ios" ? "ios-happy" : "md-happy"} size={100} />
           <Text>You already logged your information for today.</Text>
-          <TextButton onPress={this.reset}>Reset</TextButton>
+          <TextButton style={{ padding: 10 }} onPress={this.reset}>Reset</TextButton>
         </View>
       )
     }
 
     return (
-        <View>
+        <View style={styles.container}>
         <DateHeader date={(new Date()).toLocaleDateString()}/>
         {Object.keys(metaInfo).map((key) => {
           const { getIcon, type, ...rest } = metaInfo[key]
           const value = this.state[key]
 
           return (
-            <View key={key}>
+            <View key={key} style={styles.row}>
               {getIcon()}
               {type === 'slider'
                 ? <MySlider
@@ -126,6 +126,51 @@ class AddEntry extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white
+  },
+  row: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center"
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40
+  },
+  AndroidSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: "flex-end",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: "center"
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 30,
+    marginRight: 30
+  }
+});
+
 
 function mapStateToProps (state) {
   const key = timeToString()
