6c66daf004bff409961cf6c2b38660000c211d21
Armin Kuhestani <armin_kuhestani@apple.com>
2019-08-19 11:28:29 +1000

    Hooked up react-redux and dispatching of reducers

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,17 @@
-import React from "react";
-import { View } from "react-native";
+import React from "react"
+import { View } from "react-native"
 import {
   FontAwesome,
   MaterialIcons,
   MaterialCommunityIcons
-} from "@expo/vector-icons";
-import { black } from "./colors";
+} from "@expo/vector-icons"
+import { black } from "./colors"
+
+export function getDailyReminderValue () {
+  return {
+    today: "👋 Don't forget to log your data today!"
+  }
+}
 
 export function getMetricMetaInfo(metric) {
   const info = {
